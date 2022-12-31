9620983c9960987d7965547fdad8356ce41e10c0
uma255 <121157028+uma255@users.noreply.github.com>
2022-12-31 16:37:07 +0530

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,8 @@
+fo=open(r"D:\PYTHON practise 63\DAY9\mass.txt","w+")
+inpstr=input('enter text:')
+fo.write(inpstr)
+inpstr=input('enter text:')
+fo.write(inpstr)
+
+fo.close()
+print('text written to file')
---
+++ last.py
@@ -0,0 +1,4 @@
+for i in range(33,-7,-5):
+    if i%3==0:
+        print("yes",i)
+    elif
